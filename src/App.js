8619e393983a7b8bdfbb74ab5b40d7f9424be2c1
Cesar Vangelis Cachay Arbildo <cesarcachay1996@gmail.com>
2020-01-12 23:56:45 -0500

    Add navigation in MenuItem component using withRouterHOC (#1)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from "react";
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
